60af840b369957b30ed8b80519cc2217b2809f83
maksin82 <maksin82@gmail.com>
2023-10-03 01:48:02 +0300

    hw11

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -3,69 +3,74 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class WebTest {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void before() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterMethod
+    public void after() {
+       driver.quit();
+    }
+
     //TC_11_01
     @Test
     public void testHead() {
-        WebDriver driver = new FirefoxDriver();
+        // WebDriver driver = new FirefoxDriver();
         driver.get("http://www.99-bottles-of-beer.net/");
 
         WebElement header = driver.findElement(By.xpath("/html/body/div/div[1]/h1"));
-        String header1 = header.getText();
-        Assert.assertEquals(header1, "99 Bottles of Beer");
+        //String header1 = header.getText();
+        Assert.assertEquals(header.getText(), "99 Bottles of Beer");
 
-        driver.quit();
+        //driver.quit();
     }
 
     //TC_11_02
     @Test
     public void testSubLanguage() {
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.99-bottles-of-beer.net/");
 
         WebElement lastItem = driver.findElement(By.xpath
                 ("/html/body/div/div[2]/ul[1]/li[6]/a"));
         String value = lastItem.getText();
         Assert.assertEquals(value, ("Submit new Language").toUpperCase());
-
-        driver.quit();
     }
 
     //TC_11_03
     @Test
     public void testMenuSubLang() {
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.99-bottles-of-beer.net/submitnewlanguage.html");
 
         WebElement lastItem = driver.findElement(By.xpath
                 ("/html/body/div/div[2]/ul[2]/li/a"));
         String value = lastItem.getText();
         Assert.assertEquals(value, "Submit New Language");
-
-        driver.quit();
     }
 
     //TC_11_04
     @Test
     public void testMenuItem() {
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.99-bottles-of-beer.net/abc.html");
 
         WebElement header = driver.findElement(By.xpath
                 ("/html/body/div/div[2]/ul[2]/li[1]/a"));
         String item = header.getText();
         Assert.assertEquals(item, "0-9");
-
-        driver.quit();
     }
 
     //TC_11_06
     @Test
     public void testConfirmNames() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://www.99-bottles-of-beer.net/team.html");
 
         WebElement headerName1 = driver.findElement(By.xpath
@@ -80,17 +85,22 @@ public class WebTest {
                 ("/html/body/div/div[3]/h3[3]"));
         String name3 = headerName3.getText();
         Assert.assertEquals(name3, "Stefan Scheler");
-
-        driver.quit();
     }
 
-    //TC_11_07 что надпись об исключении красным цветом
-
+    //TC_11_07 что надпись об исключении красным цветом и с маленькой буквы
+    @Test
+    public void testInactive() {
+        driver.get("http://www.99-bottles-of-beer.net/team.html");
+        WebElement header = driver.findElement(By.xpath
+                ("/html/body/div/div[3]/p[7]/font/b"));
+        Assert.assertTrue(header.getText().toLowerCase().equals(header.getText()));
+        Assert.assertEquals(header.getCssValue("color"), "rgb(255, 0, 0)");
+    }
 
     //TC_11_11
     @Test
     public void testErrorFild() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://www.99-bottles-of-beer.net/submitnewlanguage.html");
 
         WebElement serchButton = driver.findElement(By.xpath("/html/body/div/div[3]/form/p/input[8]"));
@@ -105,15 +115,13 @@ public class WebTest {
                 ("/html/body/div/div[3]/p"));
         String item = header.getText();
         Assert.assertEquals(item, "Error: Precondition failed - Incomplete Input.");
-
-        driver.quit();
     }
 
     //TC_11_12
 
     @Test
     public void testPrecondition() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://www.99-bottles-of-beer.net/submitnewlanguage.html");
 
         WebElement serchButton = driver.findElement(By.xpath("/html/body/div/div[3]/form/p/input[8]"));
@@ -136,13 +144,12 @@ public class WebTest {
             word = "true";
         }
         Assert.assertEquals(word, "true");
-        driver.quit();
     }
 
     //TC_11_13
     @Test
     public void testText() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://www.99-bottles-of-beer.net/submitnewlanguage.html");
 
         WebElement text = driver.findElement(By.xpath
@@ -154,14 +161,12 @@ public class WebTest {
                 "your language will show up on this page. We don't have the time" +
                 " to mess around with fixing your descriptions etc. Thanks for " +
                 "your understanding.");
-
-        driver.quit();
     }
 
     //TC_11_14
     @Test
     public void testTable() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://www.99-bottles-of-beer.net/");
 
         WebElement button = driver.findElement(By.xpath
@@ -176,15 +181,12 @@ public class WebTest {
                 ("/html/body/div/div[3]/table/tbody/tr[1]/th[2]"));
         String elementAuthor = element2.getText();
         Assert.assertEquals(elementAuthor, "Author");
-
-        driver.quit();
-
     }
 
     //TC_11_15
     @Test
     public void testNoNewComments() {
-        WebDriver driver = new FirefoxDriver();
+
         driver.get("http://www.99-bottles-of-beer.net/");
 
         WebElement buttonTopList = driver.findElement(By.xpath
@@ -199,13 +201,12 @@ public class WebTest {
         String value = header.getText();
         Assert.assertEquals(value, "");
 
-        driver.quit();
+
     }
 
     //TC_11_21
     @Test
     public void testRedFontWiteBold() {
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.99-bottles-of-beer.net/submitnewlanguage.html");
 
         WebElement header = driver.findElement(By.xpath
@@ -219,8 +220,6 @@ public class WebTest {
         Assert.assertEquals(fontBold, "b");
         Assert.assertEquals(letterCapital, letterCapital.toUpperCase());
         Assert.assertEquals(color, "rgba(0, 0, 0, 0)");
-
-        driver.quit();
     }
 }
 
